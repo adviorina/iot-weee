2750d8889a1c6b8694d39e04fd30cd4c07323c6b
adviorina <149464114+adviorina@users.noreply.github.com>
2024-05-29 22:04:27 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+//Header Toggle Menu 
+let navLinks = document.getElementById("navLinks");
+function showMenu(){
+    navLinks.style.right = "0";
+}
+function hideMenu(){
+    navLinks.style.right = "-200px";
+}
+
+//Counter Up
+const counters = document.querySelectorAll(".counters span");
+const container = document.querySelector(".counters");
+let activated = false;
+window.addEventListener("scroll", () => {
+    if(scrollY > container.offsetTop - container.offsetHeight - 200 && activated === false){
+        counters.forEach(counter => {
+            counter.innerText = 0;
+            let count = 0;
+            function updateCount(){
+                const target = parseInt(counter.dataset.count);
+                if(count<target){
+                    count++
+                    counter.innerText = count;
+                    setTimeout(updateCount, 10);
+                }
+                else{
+                    counter.innerText = target;
+                }
+            }
+            updateCount();
+            activated = true;
+        }); 
+    }
+    else if(
+        scrollY < container.offsetTop - container.offsetHeight - 500 || scrollY === 0 && activated === true)
+        {
+            counters.forEach(counter =>{
+                counter.innerText = 0;
+            });
+            activated = false;
+        }
+    });
